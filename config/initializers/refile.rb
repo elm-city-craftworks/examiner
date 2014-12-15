6c21849532fe789f86776d93382014af36c5ed39
Gregory Brown <gregory.t.brown@gmail.com>
2014-12-15 10:46:34 -0500

    Limit file size to 200kb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,6 +4,7 @@ aws = {
   access_key_id: ENV["AWS_ACCESS_KEY"],
   secret_access_key: ENV["AWS_SECRET_KEY"],
   bucket: ENV["AWS_BUCKET"],
+  max_size: 200.kilobytes
 }
 
 Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
